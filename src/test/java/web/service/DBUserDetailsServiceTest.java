0e3f7bd1a8e0e03724ef39dfa1bb6634d46f205e
Sergei <sergei.ivanov79@list.ru>
2023-03-09 16:28:53 +0300

    add test

---
+++ src/test/java/web/service/DBUserDetailsServiceTest.java
@@ -0,0 +1,17 @@
+package web.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DBUserDetailsServiceTest {
+
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
+    @Test
+    public void test2() {
+        assertTrue(true);
+    }
+}
